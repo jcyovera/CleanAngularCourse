893a49ed7c44e72dbd428bf39d97e4973a90e662
jcyovera <jcyovera@gmail.com>
2016-04-13 17:26:59 -0500

    Directive

--- Testing/app/tests/blog/postDirective.spec.js
+++ Testing/app/tests/blog/postDirective.spec.js
@@ -1,7 +1,7 @@
 describe("post Directive", function () {
     'use strict';
 
-    var $scope, $compile, $window, postTemplate,$httpBackend,postController,$controller,blogFactory,$route;
+    var $scope, $compile, postTemplate,$httpBackend,$controller,blogFactory,$route,expectedTemplate,$q;
 
     beforeEach(function () {
         postTemplate = '<div post-directive post-value="selectedPost" show-post="showPosts">';
@@ -9,44 +9,111 @@ describe("post Directive", function () {
 
     describe("directive", function () {
         beforeEach(function () {
-          blogFactory={};
-            module('myApp.blog');
+            module('myApp.blog', [
+               '$controllerProvider', function($controllerProvider) {
+                   $controllerProvider.register('PostController', function () {
+                       // Fake controller stub.
+                       this.backPage = jasmine.createSpy('PostController.backPage');
+                       this.initialize = jasmine.createSpy('PostController.initialize');
+                       this.getComments = jasmine.createSpy('PostController.getComments');
+                   });
+               }
+           ]);
 
             inject(['$injector', function ($injector) {
-              $controller = $injector.get('$controller');
                 $scope = $injector.get('$rootScope').$new();
                 $compile = $injector.get('$compile');
-                $window = $injector.get('$window');
                 $httpBackend = $injector.get('$httpBackend');
-                $route = $injector.get('$route');
-            }]);
-            $httpBackend.whenGET('blog/post/postTemplate.html').respond("<div>{{post.postValue.id}}</div>");
-            postController = $controller('PostController'){
-              $scope: $scope,
-              blogFactory:blogFactory,
-              $route:$route
-            };
-        });
 
+            }]);
+            expectedTemplate = "<div>TEST</div>";
+            $httpBackend.whenGET('blog/post/postTemplate.html').respond(expectedTemplate);
+        });
+        afterEach(function() {
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+        });
+        it("should compile the directive", function () {
+            // Act
+            var postElement = $compile(postTemplate)($scope);
+            $httpBackend.flush(1);
+            // Assert
+            expect(postElement.html()).toContain(expectedTemplate);
+
+        });
         it("should call the controller's 'initialize' function when the directive initializes", function () {
+            // Act
+            var postElement = $compile(postTemplate)($scope);
+            $httpBackend.flush(1);
+            var postController = postElement.controller('postDirective');
+            $scope.$digest();
+            // Assert
+            expect(postController.initialize).toBeDefined();
+
+        });
+        it("should call the controller's 'getComment' function when the directive update showPost attribute", function () {
           $scope.selectedPost={};
           $scope.showPosts=false;
             // Act
             var postElement = $compile(postTemplate)($scope);
-            //var postController = postElement.controller('postDirective');
+            $httpBackend.flush(1);
+            var postController = postElement.controller('postDirective');
             $scope.$digest();
             // Assert
-            expect(postController.initialize).toHaveBeenCalled();
+            expect(postController.getComments).toHaveBeenCalled();
 
-        });
-
-        xit("shouldn't call the controller's 'resize' function when the directive initializes", function () {
-            // Act
-            var postElement = $compile(postTemplate)($scope);
-            //var postController = postElement.controller('postDirective');
-            // Assert
-            expect(postController.getComments).not.toHaveBeenCalled();
         });
 
     });
+
+    describe("controller", function () {
+        var PostController, $httpBackend,deferredGetcomments;
+
+        beforeEach(function () {
+
+            module('myApp.blog');
+
+            inject(['$injector', function ($injector) {
+                $scope = $injector.get('$rootScope').$new();
+                $compile = $injector.get('$compile');
+                $controller = $injector.get('$controller');
+                $q = $injector.get('$q');
+                $httpBackend = $injector.get('$httpBackend');
+            }]);
+            deferredGetcomments = $q.defer();
+            blogFactory={
+               getComments : jasmine.createSpy('blogFactory.getComments').and.returnValue(deferredGetcomments.promise)
+            };
+            $route={
+              reload:jasmine.createSpy('$route.reload')
+            }
+            PostController = $controller('PostController as post', {
+              $scope:$scope,
+              blogFactory:blogFactory,
+              $route :$route
+            });
+            expectedTemplate = "<div>TEST</div>";
+            $httpBackend.whenGET('blog/post/postTemplate.html').respond(expectedTemplate);
+        });
+        it("should call the method backPage", function () {
+          //Act
+            var postElement = $compile(postTemplate)($scope);
+            $httpBackend.flush(1);
+            var postController = postElement.controller('postDirective');
+            PostController.backPage();
+            // Assert
+            expect($route.reload).toHaveBeenCalled();
+        });
+        it("should call the method initialize", function () {
+          //Act
+            var postElement = $compile(postTemplate)($scope);
+            $httpBackend.flush(1);
+            var postController = postElement.controller('postDirective');
+            PostController.initialize();
+            // Assert
+            expect(PostController.postValue.comments).toEqual([]);
+        });
+
+      });
+
   });
--- Testing/e2e-tests/scenarios.js
+++ Testing/e2e-tests/scenarios.js
@@ -7,14 +7,14 @@ describe('my app', function() {
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/example");
   });
 
 
   describe('view1', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/example');
     });
 
 
@@ -29,7 +29,7 @@ describe('my app', function() {
   describe('view2', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view2');
+      browser.get('index.html#/blog');
     });
 
 
